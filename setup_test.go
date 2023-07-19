0309937d5ab40cfb71e3bcb8a30a796245938886
albertosouza <alberto.souza.99@gmail.com>
2023-07-18 22:56:07 -0300

    Update test data folder

--- setup_test.go
+++ setup_test.go
@@ -14,7 +14,7 @@ import (
 func TestMain(m *testing.M) {
 	r := approvals.UseReporter(reporters.NewVSCodeReporter())
 	defer r.Close()
-	approvals.UseFolder("testdata")
+	approvals.UseFolder("_testdata")
 
 	os.Exit(m.Run())
 
